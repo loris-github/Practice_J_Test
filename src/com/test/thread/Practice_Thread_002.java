a76a6def86aa92526e8b3317b2db007cd5c61a95
loris-github <laotao200437@gmail.com>
2014-08-29 18:15:17 +0800

    了解Synchronized
    还没完成回家继续搞定，要求访问同一个资源，然后看起效果

--- src/com/test/thread/Practice_Thread_002.java
+++ src/com/test/thread/Practice_Thread_002.java
@@ -1,5 +1,61 @@
 package com.test.thread;
 
-public class Practice_Thread_002 {
+public class Practice_Thread_002 implements Runnable {
+	
+	public static String str ="默认";
+
+	public void run() {		
+		for(int i = 0;i<20;i++){
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println("The NUM is :"+Practice_Thread_002.str);
+			}
+		
+		System.out.print("打印结束");
+	}
+	
+	public void m1(){
+		
+		str = "百日依山尽";
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void m2(){
+
+		str = "黄河入海流";
+		
+
+	}
+	
+	public static void main(String[] args){
+		
+		Practice_Thread_002 pt = new Practice_Thread_002();
+		Thread t1 = new Thread(pt);
+		Thread t2 = new Thread(pt);
+		System.out.println("打印开始：");
+		t1.start();
+		t2.start();
+		
+		try {
+				Thread.sleep(3000);
+		} catch (InterruptedException e) {
+				e.printStackTrace();
+		}
+		
+		pt.m1();
+		pt.m2();
+		
+	}
 
 }
