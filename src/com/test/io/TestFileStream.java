cd05615a20068125a2da50c14ffd247f93471b8f
loris-github <laotao200437@gmail.com>
2014-09-04 16:51:40 +0800

    修乱码修复-2
    乱码修正完毕，加入了文件编码判断
    
    但是如果FileInputStream读了3个字节存在byte[]中做判断后，套在上边的BufferedReader后用readline()读取后会相应少3个字节，不知道为啥。所以将getCode()中加入了自己的fis，之后，问题解决。

--- src/com/test/io/TestFileStream.java
+++ src/com/test/io/TestFileStream.java
@@ -5,35 +5,74 @@ import java.io.*;
 public class TestFileStream {
 	
 	String tmp = null;
+	StringBuffer content = new StringBuffer();
+	
 	boolean reading = false;
 	boolean writing = false;
 	boolean writeFinish = false;
 	boolean readFinish = false;
 	
-	String addressRead = "D:\\MyCode\\myeclipse workspace\\Test\\src\\FileDemo.java";
-	String addreassWrite = "D:\\Devuser\\Desktop\\f1.html";
+	String codeFormat = "GBK";	
+	String addressRead = "D:\\Devuser\\Desktop\\f.html";
+	String addressWrite = "D:\\Devuser\\Desktop\\f1.html";
 		
 	BufferedReader br = null;
-	BufferedOutputStream bos = null;
+	BufferedWriter bw = null;
+	BufferedOutputStream bos = null;	
 	
-	public void readFile(String address){
-			try {
-				br = new BufferedReader(new InputStreamReader(new FileInputStream(address),"UTF-8"));
-				reading = true;
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}			
-		 catch (FileNotFoundException e) {
-			System.out.println("木有找到文件 ╮(╯▽╰)╭");
+	public String getFormat (String address){		
+		byte[] head = new byte[3];
+		FileInputStream fis = null;
+		
+		try {
+		fis = new FileInputStream(address);
+		fis.read(head);
+		
+		} catch (FileNotFoundException e) {
+			System.out.println("木有找到文件╮(╯▽╰)╭");
+			System.exit(-1);
+			
+		} catch (IOException e) {
+			System.out.println("念书少没读好╮(╯▽╰)╭");
 			System.exit(-1);
 		}
 		
-		try{			
+		if (head[0] == -1 && head[1] == -2 )    
+			codeFormat = "UTF-16";    
+        else if (head[0] == -2 && head[1] == -1 )    
+        	codeFormat = "Unicode";    
+        else if(head[0]==-17 && head[1]==-69 && head[2] ==-65)    
+        	codeFormat = "UTF-8";
+		
+		try {
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return codeFormat;
+	}
+	
+	public void excuteFile(String address){			
+			try {
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(addressRead),getFormat(addressRead)));				
+				reading = true;
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+			System.out.println("木有找到文件 ╮(╯▽╰)╭");
+			System.exit(-1);
+		    } 
+		
+		try{
 			if(reading){
 				while((tmp=br.readLine())!=null){
-					System.out.print(tmp+'\n');
+					content.append(tmp+'\n');
 				}					
 			}
+			
+			System.out.println(content.toString());
+			tmp = null;
 			br.close();
 			readFinish = true;
 			System.out.println("read finish!");
@@ -50,13 +89,13 @@ public class TestFileStream {
 			bw = new BufferedWriter (new FileWriter(address));
 			writing = true;
 		} catch (IOException e) {
-			System.out.println("木有找到文件 ╮(╯▽╰)╭");
+			System.out.println("写不了！原因不知道");
 			System.exit(-1);
 		}
 		
 		try{			
 			if(writing&&readFinish){
-				bw.write(tmp);					
+				bw.write(content.toString());
 			}
 			bw.close();
 			writeFinish = true;
@@ -71,20 +110,20 @@ public class TestFileStream {
 	
 	public static void main(String[] args) {
 		TestFileStream tfs = new TestFileStream();
-		tfs.readFile(tfs.addressRead);
-		//tfs.writeFile(tfs.addreassWrite);
+		tfs.excuteFile(tfs.addressRead);
+		tfs.writeFile(tfs.addressWrite);
 		
-		/*if(tfs.writeFinish){
+		if(tfs.writeFinish){
 			System.out.print("all done! exit in 5 sec!");
 		}
 		
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			System.exit(-1);
 		}
 		
-		System.exit(-1);*/
+		System.exit(-1);
 	}
 
 }
